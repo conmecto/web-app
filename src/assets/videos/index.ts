b265a58e68427327744cb1be9a493507c100c1e7
conmecto <conmecto@gmail.com>
2024-10-24 00:08:10 +0530

    fix: removed unnecessary files

--- src/assets/videos/index.ts
+++
@@ -1,9 +0,0 @@
-import sampleVideo1 from './sampleVideo1.mp4'
-import sampleVideo2 from './sampleVideo2.mp4'
-import sampleVideo3 from './sampleVideo3.mp4'
-
-export { 
-  sampleVideo1,
-  sampleVideo2,
-  sampleVideo3
-}
